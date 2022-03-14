8f624d78f3a6f6d82899f7894724ad4df5241f9f
tuyikai <101246809+tuyikai@users.noreply.github.com>
2022-03-14 18:31:54 +0800

    Add files via upload

---
+++ Practical5/pizza_party.py
@@ -0,0 +1,14 @@
+n =1
+a=n**2
+b=n
+c=2
+d=a+b+c  
+e=d/2
+while e <= 64:
+    a=n**2
+    b=n
+    c=2
+    d=a+b+c  
+    e=d/2
+    n=n+1        
+print("需要至少切",n,"刀")
---
+++ Practical5/triangle.py
@@ -0,0 +1,6 @@
+
+s=0
+print(s)
+for i in range (1,10):
+    s= s+ i
+    print ("s",s)
