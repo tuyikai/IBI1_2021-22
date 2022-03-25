4c030bd6d2fdea1a0def35209cd6ec0f0b25eeb6
tuyikai <101246809+tuyikai@users.noreply.github.com>
2022-03-25 11:25:05 +0800

    Add files via upload

--- Practical6/list manipulation.py
+++ Practical6/list manipulation.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-a = (45,36,86,57,53,92,65,45)
+a = (45,36,86,57,53,92,65,45)# the data
 fig, ax = plt.subplots()
-VP = ax.boxplot(a)
-plt.show()
+VP = ax.boxplot(a)# the graph model is a boxplot
+plt.show()# show the boxplot
--- Practical6/the effect of paternal age on offspring health.py
+++ Practical6/the effect of paternal age on offspring health.py
@@ -13,8 +13,8 @@ plt.style.use('_mpl-gallery')
 # make the data
 np.random.seed(3)
 x = (30,35,40,45,50,55,60,65,70,75)
-y = (1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94)
+y = (1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94)# the data
 
-plt.scatter(x,y)
+plt.scatter(x,y)# the graph is a scatter 
 
-plt.show()
+plt.show()# show the scatter
